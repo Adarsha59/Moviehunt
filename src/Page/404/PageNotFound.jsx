6b95cab3bd6c51091b1900bb1a3844696b1adbe4
Adarsha <92156762+Adarsha59@users.noreply.github.com>
2024-01-10 21:39:17 +0545

    Create PageNotFound.jsx

---
+++ src/Page/404/PageNotFound.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./style.scss";
+import ContentWrapper from "../../components/ContentWrapper/Wrapper";
+
+
+const PageNotFound = () => {
+    return (
+        <div className="pageNotFound">
+            <ContentWrapper>
+                <span className="bigText">404</span>
+                <span className="smallText">Page not found!</span>
+            </ContentWrapper>
+        </div>
+    );
+};
+
+export default PageNotFound;
